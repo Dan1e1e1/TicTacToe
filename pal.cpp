98113ba610277abd75254d0dcfc5ee9cd277049a
U-BSD\466442 <466442@SHS-BYBF6X3.beaverton.k12.or.us>
2026-09-10 09:54:34 -0700

    Reversing logic

--- pal.cpp
+++ pal.cpp
@@ -7,19 +7,25 @@ int main()
 {
   char str[20];
   char str2[20];
-  
+  char str3[20];
   cin.get(str, 20);
   cin.get();
-  
-  int count = 0;
-  for (int i = 0; i < 20; i++) {
-    if (str[i] != '\0') {
-      str2[count] = str[i];
-      count++;
-    }
-
+  int start = 0;
+  int end = strlen(str) - 1;
+  //inversing logic
+  while (start < end) {
+    //creating a temp variable and set that to the start 
+    char temp = str[start];
+    //set the start char to the end
+    str[start] = str[end];
+    //then set the end char to the temp and repeat 
+    str[end] = str[temp];
+    //increase start variable by one
+    start++;
+    //decrease the end variable by one
+    end--;
+    //eventually the 2 variables will meet and the while loop will end.
   }
-  cout << str << endl;
   cout << str2 << endl;
 
   return 0;
