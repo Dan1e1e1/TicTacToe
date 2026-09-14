5bfb1156b5a1e732801518eebe82fddaa7784a9f
U-BSD\466442 <466442@SHS-BYBF6X3.beaverton.k12.or.us>
2026-09-14 09:14:00 -0700

    changed str[temp] to temp and that fixed only getting half of the reversed text

--- pal.cpp
+++ pal.cpp
@@ -7,10 +7,8 @@ int main()
 {
   char str[20];
   cin.get(str, 20);
-  cin.get();
   int start = 0;
   int end = strlen(str) - 1;
-  char str2[20];
   //inversing logic
   while (start < end) {
     //creating a temp variable and set that to the start 
@@ -18,14 +16,13 @@ int main()
     //set the start char to the end
     str[start] = str[end];
     //then set the end char to the temp and repeat 
-    str[end] = str[temp];
+    str[end] = temp;
     //increase start variable by one
     start++;
     //decrease the end variable by one
     end--;
     //eventually the 2 variables will meet and the while loop will end.
   }
-  cout << strcmp(str, str2);
   cout << str;
   return 0;
 }
