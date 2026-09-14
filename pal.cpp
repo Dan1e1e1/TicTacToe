5bfc07c163d52282e11dbb68166f8914a76ae1c1
U-BSD\466442 <466442@SHS-BYBF6X3.beaverton.k12.or.us>
2026-09-14 10:17:50 -0700

    Finished the code

--- pal.cpp
+++ pal.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cstring>
 
@@ -5,10 +6,30 @@ using namespace std;
 
 int main()
 {
-  char str[20];
-  cin.get(str, 20);
+  char str[81];
+  cin.get(str, 81);
+  char str2[81];
+  int read = 0;
+  int write = 0;
+  //Remove all spaces and punctuation
+  while (str[read] != '\0') {
+    unsigned char ch = str[read];
+    if (!isspace(ch) && !ispunct(ch)) {
+        str[write] = str[read];
+        write++;
+    }
+    read++;
+  }
+  str[write] = '\0';
+  //Make all characters lowercase
+  for (int i = 0; str[i] != '\0'; i++) {
+    str[i] = tolower(str[i]);
+  }
+
+  //Copy the simplifed text to later compare them backwards and forwards
+  strcpy(str2, str);
   int start = 0;
-  int end = strlen(str) - 1;
+  int end = strlen(str) -1;
   //inversing logic
   while (start < end) {
     //creating a temp variable and set that to the start 
@@ -23,6 +44,12 @@ int main()
     end--;
     //eventually the 2 variables will meet and the while loop will end.
   }
-  cout << str;
+  cout << str << '\n';
+  int value = strcmp(str, str2);
+  if (value == 0) {
+    cout << "Palindrome";
+  } else {
+    cout << "Not a palindrome";
+  }
   return 0;
 }
