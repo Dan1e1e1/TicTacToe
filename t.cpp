e4b078334e318a78b972d3ce344e86ce4932bf9e
U-BSD\466442 <466442@SHS-BYBF6X3.beaverton.k12.or.us>
2026-09-23 10:21:56 -0700

    added a functioning turn system and placing system but users can overide other X and O.

--- t.cpp
+++ t.cpp
@@ -10,30 +10,19 @@ int main() {
     {'b', ' ', ' ', ' '},
     {'c', ' ', ' ', ' '}
   };
-  char a1 = board[1][1];
-  char a2 = board[1][2];
-  char a3 = board[1][3];
-
-  char b1 = board[2][1];
-  char b2 = board[2][2];
-  char b3 = board[2][3];
-
-  char c1 = board[3][1];
-  char c2 = board[3][2];
-  char c3 = board[3][3];
-
   int rows = 4;
   int cols = 4;
 
   char game = true;
-  int turn = 0;
+  int turn = 1;
   char turn1 = 'X';
   while (game == true) {
-    turn++;
-    if (turn = 1) {
+    if (turn == 1) {
       turn1 = 'X';
+      turn--;
     } else {
       turn1 = 'O';
+      turn++;
     }
     char input;
     int input1;
@@ -47,15 +36,50 @@ int main() {
         for (int j = 0; j < cols; j++) {
           if (input == 'a') {
             if (input1 == 1) {
-              board[1][1] = turn1;
-  	      cout << board[i][j];
-	      con = false;
+	      if (board[1][1] == ' ') {
+                board[1][1] = turn1;
+	      } else {
+		cout << "fail";
+	      }
 	    }
+	    if (input1 == 2) {
+	      board[1][2] = turn1;
+	    }
+	    if (input1 == 3) {
+	      board[1][3] = turn1;
+	    }
+	    cout << board[i][j];
+	  }
+	  else if (input == 'b') {
+	    if (input1 == 1) {
+	      board[2][1] = turn1;
+	    }
+	    if (input1 == 2) {
+	      board[2][2] = turn1;
+	    }
+	    if (input1 == 3) {
+	      board[2][3] = turn1;
+	    }
+	    cout << board[i][j];
+	  }
+	  else if (input == 'c') {
+	    if (input1 == 1) {
+	      board[3][1] = turn1;
+	    }
+	    if (input1 == 2) {
+	      board[3][2] = turn1;
+	    }
+	    if (input1 == 3) {
+	      board[3][3] = turn1;
+	    }
+	    cout << board[i][j];
 	  }
 	}
 	//Formatting the printing so its in a 4x4
 	cout << "\n";
       }
+      //stop infinite loop
+      con = false;
     }
   }
 }
